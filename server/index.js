46265b76fe1a992ef85a7aeb3234c7a53575b55e
timmmleee <timothylee3508@gmail.com>
2020-01-22 19:36:15 +0000

    completed user can view rates-backend feature

--- server/index.js
+++ server/index.js
@@ -19,7 +19,6 @@ app.get('/api/health-check', (req, res, next) => {
     .catch(err => next(err));
 });
 
-// GET request for all information
 app.get('/api/cars', (req, res, next) => {
   const getAllCarsSql = `
     SELECT *
@@ -28,6 +27,30 @@ app.get('/api/cars', (req, res, next) => {
   db.query(getAllCarsSql)
     .then(result => res.json(result.rows))
     .catch(err => next(err));
+});
+
+app.get('/api/cars', (req, res, next) => {
+  if (!req.session.userId) {
+    throw (new ClientError('UserId is not logged in!', 400));
+  }
+  const sql = `
+    SELECT "c" "userId",
+           "c" "total",
+           "c" "start-date",
+           "c" "end-date"
+           "p" "make"
+           "p" "rate"
+      FROM "rentals" as "c"
+      JOIN "cars" as "p" using "carId"
+     WHERE "c"."userId" = $1
+  `;
+  const params = [req.session.userId];
+  db.query(sql, params)
+    .then(result => res.status(200).json(result.rows))
+    .catch(err => {
+      console.err(err);
+      next(new ClientError('An unexpected error occured'), 404);
+    });
 });
 
 app.use('/api', (req, res, next) => {
