775d0df571cfa53b3747ec9e4808282f9fb04a04
AndrewRobins51413 <h.andrew.robins@gmail.com>
2020-01-23 21:31:28 +0000

    Robins re-worked details, ready for db

--- client/components/user-view-details-front-end.jsx
+++ client/components/user-view-details-front-end.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
+import dummyData from './dummy-data';
+
+// import CarListItem from './car-list-item';
 
 class CarDetailsList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      carDetails: [],
+      carDetails: dummyData[3],
       isClicked: false
     };
     this.handeClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
-    fetch('/api/cars/')
-      .then(response => response.json())
-      .then(carDetailsData => {
-        this.setState({ carDetails: carDetailsData });
-      })
-      .catch(error => `${error}`);
+    this.getCarDetails();
+  }
+
+  getCarDetails() {
+    // fetch('/api/cars/')
+    //   .then(response => response.json())
+    //   .then(carDetailsData => {
+    //     this.setState({ carDetails: carDetailsData });
+    //   })
+    //   .catch(error => console.error(error));
   }
 
   handleClick() {
@@ -24,17 +31,12 @@ class CarDetailsList extends React.Component {
   }
 
   render() {
+    const oneCarDetails = this.state.carDetails;
     return (
       <div id="root">
         <div className="container .container-fluid ">
           <div className="row">
-            <button onClick={this.props.handleClick}></button>
-            {
-              this.state.carDetails.map(carDetail => (
-                <carRenderCode key={carDetail.carDetails} carDetailData={carDetail}>
-                </carRenderCode>
-              ))
-            }
+            <button onClick={this.props.handleClick}>{oneCarDetails.model}</button>
           </div>
         </div>
       </div>
@@ -42,4 +44,14 @@ class CarDetailsList extends React.Component {
   }
 }
 
-export default CarDetailList;
+export default CarDetailsList;
+
+// rate={car.rate}
+// year={car.year}
+//
+// shortDescription={car.shortDescription}
+// topSpeed={car.topSpeed}
+// availability={car.availability}
+// mpg={car.mpg}
+// make={car.make}
+// productId={car.carId}
